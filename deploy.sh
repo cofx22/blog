5883bb853314b3b0dd47253323813574b5a6422a
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-06 21:35:12 +0100

    Force update during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 bb quickblog clean
 git clone -b deploy https://github.com/cofx22/blog.git public
+rm -rf public/*
+printf "%s" "blog.cofx.nl" > public/CNAME
 bb quickblog render
 cd public
 git add .
